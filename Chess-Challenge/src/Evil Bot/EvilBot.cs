e54f712b7e833da3d845eceb2a3a2afbbc502bf0
Steven He <therealonestig@gmail.com>
2023-10-01 04:22:47 -0700

    Compare king safety w/ or w/o

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,139 +1,177 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
     // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
     // Plays randomly otherwise.
-    public class EvilBot : IChessBot
-    {
-    // 1st row is mg, 2nd row is eg
-    long[] raw_mgeg_table = {281479271743489000, 281479271743489000, 309064394990748743, 300620111327921117, 279790451949700103, 299775385749488596, 277538677904507901, 287953247748555729, 273879438782039028, 286264372118029263, 274160905168880606, 282323709560685532, 271627643262338001, 277257250174534610, 281479271743489000, 281479271743489000, 234472568378491831, 298648828710028157, 260931422354670604, 287953462496265175, 268250205538550825, 305123723842749460, 278946069968782365, 291894142235706366, 277820114227168245, 289360652697142240, 275005308625224690, 286827369317073880, 273316269784826853, 281197874075272149, 251924308990755783, 276694076879078353, 273316514593375171, 274442216937554912, 274160991067309019, 289923774449058745, 276975831032726544, 291331110679151590, 280353393312728090, 291894004796031974, 279790477718651906, 291049472491914220, 281479336168981495, 285420037382734834, 282605236075889640, 283449686776939497, 272190584626217939, 277820045504086995, 290486651388953627, 299212233932997651, 289079233557431334, 303997557644854292, 280071978466018316, 286264539625096184, 274723825058251778, 288234821507875796, 271346060911969255, 284012516469507025, 268812790416278487, 282323696673293255, 269094304047432671, 281197844011025313, 276131191351477241, 285982901433205710, 273597972397491188, 298086484350796821, 274723704798381033, 276975916931089438, 277820024032265200, 289642286589346849, 273879331407135704, 281197869781091305, 278945885283353566, 280916304610395109, 277538630658753510, 280071905450787821, 271627613199598570, 283731135981487081, 281197719456777202, 277257039716615094, 263183497042592729, 265716527018935285, 289642041771820001, 279227454747444171, 278946100032439256, 275849797980521426, 276694111239275469, 273034915067069380, 267686993587930049, 268531233834075061, 277538561938555834, 269094243918218189, 281760776784446376, 269375779026043888, 277257301712438194, 283730951299662838,
-                             281479271743489000, 281479271743489000, 331582560637682798, 322856660266452131, 307938349056590891, 297242098077926460, 290486574078297069, 280916338971051001, 285138485095236577, 279508912546972647, 282605201714709481, 281479250268586976, 285138480800990194, 285138446440858593, 281479271743489000, 281479271743489000, 265153559884661708, 272753431497212805, 274442362964345830, 278945889577337780, 274723786403742705, 281197758110827455, 276694210025751550, 287671768476287958, 276412696393942017, 285982944385565654, 275005334394176503, 284294008624382930, 269657236821640163, 280916235889214396, 273316278374040537, 275286744943166376, 277538531874505696, 279508908250694608, 279227454750393308, 280634790978520026, 282042187337171943, 280916347559871468, 280634885468849137, 285419964367307754, 279790434768978939, 283449639529939935, 278101559138583538, 285138459325170649, 277538544759669735, 282605132994642893, 275005308622930915, 278945928233288663, 285138489391580151, 284857023004148717, 284575552322733043, 280634859698783211, 283449626645693421, 282605158765102053, 282605184536085481, 282042225991812074, 282323718148981740, 280071871089607645, 280353371836318695, 279508895366382552, 279790396113421290, 278945958297666533, 278946005543224295, 280071841025622996, 278946091443815427, 289079177719710716, 276694283040850961, 297804927768855528, 275849797979669529, 294708745973990385, 282323791164474389, 297523517218423820, 276412842423026711, 290205142052963327, 276975556152984558, 284012619548984301, 275286723469640664, 276975573329511368, 272190477251511229, 280071759419671487, 260649973141865430, 278383111424443351, 278101645038322681, 286264509557834739, 284294094526546935, 287108964554114037, 279227566420657155, 288797762873590763, 276412704984204288, 289079194899514333, 276131234301805565, 287953264927769567, 273879400127923189, 285419938596979671, 266560951947559901, 273597912264475581};
-    int[] value_mg = { 82, 237, 365, 477, 1025, 12000 };
-    int[] value_eg = { 94, 281, 297, 512, 936, 12000 };
+public class EvilBot : IChessBot
+{
+    // todo: mvv lva, iterative deepening + transposition table
+    // https://www.chessprogramming.org/King_Safety
 
-    // value of pieces by game phase from PeSTO
-    int[] value_gp = { 0, 1, 1, 2, 4, 0 };
+    // TELEMETRY //
+    // int nodes;
+    // ========= //
+
+
+    int[,,] pst = new int[2, 6, 64];
+
+    int[] value_mg = { 82, 237, 365, 477, 1025, 0 },
+          value_eg = { 94, 281, 297, 512, 936, 0 },
+          value_gp = { 0, 1, 1, 2, 4, 0 },
+          attack_units = { 1, 2, 2, 3, 5, 0 },
+          
+          bw = {0, 1};
 
     Move best_move;
 
-    short table_query(int table, int piece, int index) {
-        int ind = 384 * table + piece * 64 + index;
-        return (short)((raw_mgeg_table[ind / 4] >> (3 - ind % 4) * 16 & 0xFFFF) - 1000);
-    }
+     // max search depth, size of transposition table
+    const int max_depth = 60, tpt_size = 1 << 20;
 
-    const int max_depth = 3;
+    // zobrist key, depth, eval, bound, best move found 
+    (ulong, int, int, int, Move)[] tpt = new (ulong, int, int, int, Move)[tpt_size];
 
-    // does not store depth yet just raw evaluation
-    struct Transposition
+    public EvilBot()
     {
-        public ulong zobrist;
-        public Move move;
-        public int eval;
+        // raw data in constructor saves 99,840 bytes after gc lmao
+        // 1st row is mg, 2nd row is eg
+        decimal[] raw_mgeg_table = {324454911572041844052197376m, 75076251637434867814284861m, 29123027899232255186595905m, 679463589935167471377853969m, 622764552386721285493885210m, 9783870073220884m, 853792511368197730866298880m, 660107126339636692766873359m, 42355312132823125893347631m, 31507670913842921742821907m, 41193295289308132637156892m, 666184410397949141357830169m, 11019859289303915835304809m, 651696172032278943469292370m, 121058234499184519289533982m, 623839121419587375156298789m, 36267811662495560175459078m, 75696286717145482010127m, 649257115644407590980692487m, 104114097994879113462164263m, 47172125096491727721087003m, 62899666870273375367088154m, 635939857660055141050500632m, 699991661452951898981543430m, 651703762498292834060345644m, 29151173424785625276490241m, 139102041054936954514987067m, 138040011692296212609002524m, 683085603252313994626742043m, 625066618091819514183947529m, 36305572174512961990690053m, 741220517434551086583382787m, 621524939860012836400934721m, 657665316748116284395360020m, 680769522489397229590678804m, 743647989619289918984833294m, 16932298947359490929466638m, 749591013482441976939708680m, 1885485064m,
+                                    419128914820612491296374784m, 162398385016552208672296094m, 10889823461391402009127480m, 621404478098643831804404241m, 19406643487212640098848260m, 9253627301729288m, 685609957376361150176821248m, 746023164048269947162773311m, 8545505272307244887320856m, 60524270313517289798380566m, 626270941214619540590174736m, 726675582325096560821153044m, 671022900017996762837837597m, 648019698721222534142759179m, 15727850803858238216477443m, 4849962864146558573348864m, 627491617637458285197264646m, 622632385744292846723404808m, 658909485323780599656092164m, 12127148025175477157503749m, 16958045854339547543116299m, 2479270154810991257324039m, 634709704167874128796451330m, 658904827232388861928806152m, 6089524068962989632523526m, 65386094019524721194172931m, 60720566972174612052846107m, 21850713382660521851879454m, 68994107384350856324328451m, 14580560447633344444522003m, 675867604947451165696074249m, 719407971043805646467514660m, 42371590137476262778259274m, 36376545995402102452789774m, 79912135087302895496878612m, 645609101597349316523460122m, 646905261709119178293511955m, 646877163543929968236435972m, 40204639516m };
 
-        public Transposition(ulong _zobrist, Move _move, int _eval)
-        {
-            zobrist = _zobrist;
-            move = _move;
-            eval = _eval;
+        // pre process the pst to reduce method calls to query the raw table
+
+        foreach (int phase in bw) {
+            // only go to 384, because last 6 are empty 0s off the board to make multiples of 10
+            for (int i = 0; i < 384; i++) {
+                BigInteger segment = (BigInteger.Parse(raw_mgeg_table[phase * 39 + i / 10].ToString()) >> 9 * (i % 10)) & 0x1FF;
+
+                pst[phase, i / 64, i % 64] = (short)((segment & 0xFF) * ((segment & (1 << 8)) != 0 ? -1 : 1));
+            }
         }
     }
-
-    const int TTotal = (1 << 20);
-    Transposition[] tt = new Transposition[TTotal];
-    
-    int tb_ind(int n)
-    {
-        return (7 - n / 8) * 8 + (n % 8);
-    }
-
     int Eval(Board board)
     {
-        Transposition cur = tt[board.ZobristKey % TTotal];
-        if (cur.zobrist == board.ZobristKey)
-        {
-            return cur.eval;
-        }
+        int tb_ind(int n) => (7 - n / 8) * 8 + (n % 8);
+        // int safety_table(int ind) => (ind > 60 ? 500 : ind * ind / 8);
 
         int gamePhase = 0, mg_sum = 0, eg_sum = 0;
 
-        PieceList[] all_pl = board.GetAllPieceLists();
+        foreach (int color in bw) {
+            bool is_white = color == 1;
+            ulong opp_king_squares = BitboardHelper.GetKingAttacks(board.GetKingSquare(!is_white));
 
-        foreach (PieceList pl in all_pl)
-        {
-            int p_type_ind = (int)pl.TypeOfPieceInList - 1;
+            for (int i = 0; i < 6; i++) {
+                PieceList pl = board.GetPieceList((PieceType)(i + 1), is_white);
+                gamePhase += value_gp[i] * pl.Count;
 
-            for (int i = 0; i < pl.Count; i++)
-            {
-                Piece p = pl.GetPiece(i);
+                foreach (Piece p in pl) {
+                    int ind = is_white ? tb_ind(p.Square.Index) : p.Square.Index;
 
-                gamePhase += value_gp[p_type_ind];
-
-                // if white flip board, otherwise dont (because arrays listed in POV of white)
-                int ind = p.IsWhite ? tb_ind(p.Square.Index) : p.Square.Index;
-
-                int neg = board.IsWhiteToMove == p.IsWhite ? 1 : -1;
-
-                mg_sum += (table_query(0, p_type_ind, ind) + value_mg[p_type_ind]) * neg;
-                eg_sum += (table_query(1, p_type_ind, ind) + value_eg[p_type_ind]) * neg;
+                    mg_sum += pst[0, i, ind] + value_mg[i];
+                    eg_sum += pst[1, i, ind] + value_eg[i];
+                    // Console.WriteLine($"mg {p_type_ind} {ind} {pst[0, p_type_ind, ind]}");
+                
+                }
             }
+
+            mg_sum *= -1;
+            eg_sum *= -1;
         }
 
+        // in case of promotion resulting in > 24
         gamePhase = Math.Min(gamePhase, 24);
-
         int sum = (mg_sum * gamePhase + eg_sum * (24 - gamePhase)) / 24;
-        // int sum = mg_sum;
-        cur = new Transposition(board.ZobristKey, Move.NullMove, sum);
 
-        return sum;
+        return board.IsWhiteToMove ? -sum : sum;
     }
 
-    public int Negamax(Board board, int depth, int alpha, int beta, Timer timer)
+    int Negamax(Board board, int depth, int alpha, int beta, Timer timer, int ply)
     {
-        if (board.IsInCheckmate())
-        {
-            return -10000;
-        }
+        // nodes++;
 
-        if (depth != max_depth && board.IsRepeatedPosition()) {
-            return 0;
-        }
+        if (board.IsInCheckmate()) return -10000 + ply;
+        if (board.IsDraw()) return 0;
+
+        if (ply > 0 && board.IsRepeatedPosition()) return 0;
+
+        ulong bkey = board.ZobristKey;
+        ulong bkey_mod = bkey % tpt_size;
 
         int bestValue = int.MinValue;
-        Move[] consider_moves;
 
-        if (depth <= 0)
+        bool quiescence = depth <= 0;
+
+        var cur_tpt = tpt[bkey_mod];
+
+        // 1. ensure not on the root node
+        // 2. ensure not a collision with different key
+        // 3. ensure stored info calculated deeper
+        // 4. check if alternative better score was reached
+        if (ply > 0 && cur_tpt.Item1 == bkey && cur_tpt.Item2 >= depth &&
+            (cur_tpt.Item4 == 3 || 
+             cur_tpt.Item4 == 2 && cur_tpt.Item3 >= beta ||
+             cur_tpt.Item4 == 1 && cur_tpt.Item3 <= alpha)) {
+                return cur_tpt.Item3;
+             }
+        
+        if (quiescence)
         {
-            consider_moves = board.GetLegalMoves(true);
-
             bestValue = Eval(board);
 
-            if (bestValue >= beta || timer.MillisecondsElapsedThisTurn > timer.MillisecondsRemaining / 30) {
+            if (bestValue >= beta) {
                 return bestValue;
             }
+            
+            alpha = Math.Max(alpha, bestValue);
+        }
 
-            if (alpha < bestValue) {
-                alpha = bestValue;
+        int f_alpha = alpha;
+
+        Move[] consider_moves = board.GetLegalMoves(quiescence);
+        int[] priority = new int[consider_moves.Length];
+
+        for (int i = 0; i < consider_moves.Length; i++) {
+            Move cur = consider_moves[i];
+
+            priority[i] = 1<<10;
+
+            // start search with last found best
+            if (consider_moves[i] == cur_tpt.Item5 && cur_tpt.Item1 == bkey)
+                priority[i] = -10000;
+
+            if (consider_moves[i].IsCapture) {
+                // maybe consider adding value of check later?
+
+                // subtracting backwards cause sort in ascending
+                priority[i] = value_mg[(int)cur.MovePieceType - 1] - value_mg[(int)cur.CapturePieceType - 1];
             }
         }
-        else {
-            consider_moves = board.GetLegalMoves();
-        }
+
+        // maybe sorting on the fly is better when alpha >= beta
+        // for now pre sort everything
+
+        Array.Sort(priority, consider_moves);
+
+        Move local_best = Move.NullMove;
 
         foreach (Move move in consider_moves)
         {
+            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 35) {
+                return bestValue;
+            }
+                
             board.MakeMove(move);
-            int value = -Negamax(board, depth - 1, -beta, -alpha, timer);
+            int value = -Negamax(board, depth - 1, -beta, -alpha, timer, ply + 1);
             board.UndoMove(move);
 
-
-            if (bestValue < value) {
+            if (bestValue < value)
+            {
                 bestValue = value;
-
-                if (depth == max_depth) {
-                    best_move = move;
-                }
+                local_best = move;
             }
 
             alpha = Math.Max(alpha, value);
@@ -144,18 +182,26 @@ namespace ChessChallenge.Example
             }
         }
 
+        if (ply == 0)
+        {
+            best_move = local_best;
+        }
+
+        tpt[bkey_mod] = (bkey, depth, bestValue, bestValue >= beta ? 2 : bestValue > f_alpha ? 3 : 1, local_best);
+
         return bestValue;
     }
 
     public Move Think(Board board, Timer timer)
     {
-        // Move[] allMoves = board.GetLegalMoves();
+        // Console.WriteLine("board: " + Eval(board));
+        for (int d = 1; d <= max_depth && timer.MillisecondsElapsedThisTurn < timer.MillisecondsRemaining / 35; d++) {
+            // nodes = 0;
+            int eval = Negamax(board, d, -10000, 10000, timer, 0);
 
-        Move moveToPlay = Move.NullMove;
+            // Console.WriteLine($"Depth: {d}; \x1b[31m{best_move}\x1b[0m: {eval} ; Nodes searched: {nodes} @ {timer.MillisecondsElapsedThisTurn}ms");
+        }
 
-        Negamax(board, max_depth, -10000, 10000, timer);
-
-        return best_move == Move.NullMove ? board.GetLegalMoves()[0] : best_move;
+        return best_move;
     }
-}
-}
+}}
